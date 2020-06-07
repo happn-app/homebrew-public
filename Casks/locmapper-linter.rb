81ca1cd3c197e6d11ddfce953af1a8783b958151
Frizlab <fload@me.com>
2020-06-07 14:57:21 +0200

    Add LocMapper Linter 1.1.1 app

---
+++ Casks/locmapper-linter.rb
@@ -0,0 +1,14 @@
+cask 'locmapper-linter' do
+  version '1.1.1'
+  sha256 'e3e429f1ea73796a8665d3146ae77cce1472a873c32eb2f952fe218dfa339296'
+
+  url "https://github.com/happn-tech/LocMapper/releases/download/LocMapper_Linter-5/LocMapper-Linter.zip"
+  name 'LocMapper Linter'
+  homepage 'https://github.com/happn-tech/LocMapper'
+
+  app 'LocMapper Linter.app'
+
+  zap trash: [
+               '~/Library/Containers/com.happn.LocMapper-Linter/',
+             ]
+end
