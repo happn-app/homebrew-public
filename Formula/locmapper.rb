6314dbbe111b2238c3e7ae02f5c2a198a714b0e3
Frizlab <fload@me.com>
2020-12-11 11:53:23 +0100

    Try and remove Xcode dependency for locmapper

--- Formula/locmapper.rb
+++ Formula/locmapper.rb
@@ -4,8 +4,6 @@ class Locmapper < Formula
   url "https://github.com/happn-tech/LocMapper.git", :using => :git, :tag => "1.3", :revision => "6a9b717a6601badd7454f5a7a6ed59a5904fee98"
   head "https://github.com/happn-tech/LocMapper.git", :using => :git, :branch => "develop"
 
-  depends_on :xcode => ["12.2", :build]
-
   def install
     system "swift", "build", "--disable-sandbox", "--configuration", "release", "--disable-automatic-resolution"
 
