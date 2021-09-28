24666d3f7bfd8e0243b46d20d51ea6d99d9fa2ab
Frizlab <fload@me.com>
2021-09-28 16:41:00 +0200

    Install officectl dependencies

--- Formula/officectl.rb
+++ Formula/officectl.rb
@@ -2,21 +2,20 @@ class Officectl < Formula
   desc "Manage multiple user directories (LDAP, Google Apps, etc.)"
   homepage "https://github.com/happn-app/officectl"
   url "https://github.com/happn-tech/officectl.git", :using => :git, :tag => "0.7.6", :revision => "3886bcf69666d99461f8478b8fb216ace2717009"
+  revision 2
   head "https://github.com/happn-tech/officectl.git", :using => :git, :branch => "develop"
 
   depends_on :xcode => ["12.4", :build]
 
-  depends_on "openldap"
-  depends_on "openssl"
-
   def install
     # openldap does not support pkg-config, so we have to explicitly add the
     # headers search path and link options to swift build.
-    system "swift", "build", "--disable-sandbox", "--configuration", "release", "--disable-automatic-resolution",
-           "-Xcc", "-I#{HOMEBREW_PREFIX}/opt/openldap/include", "-Xlinker", "-L#{HOMEBREW_PREFIX}/opt/openldap/lib"
+    system "swift", "build", "--disable-sandbox", "--configuration", "release", "--disable-automatic-resolution"
 
     bin.install "./.build/release/officectl"
     bin.install "./.build/release/officectl_odproxy"
+    bin.install "./.build/release/COpenSSL.framework"
+    bin.install "./.build/release/COpenLDAP.framework"
     (share+"officectl").install "Public"
     (share+"officectl").install "Resources"
   end
