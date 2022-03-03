675d4a099b21b35c5f1aaf8ae03460b55eb1cf74
Frizlab <fload@me.com>
2022-03-03 15:48:23 +0100

    Add completion installation for locmapper

--- Formula/locmapper.rb
+++ Formula/locmapper.rb
@@ -1,15 +1,30 @@
 class Locmapper < Formula
-  desc "A utility for working w/ LocMapper (*.lcm) files"
+  desc "Utility for working w/ LocMapper (*.lcm) files"
   homepage "https://github.com/happn-tech/LocMapper"
-  url "https://github.com/happn-tech/LocMapper.git", :using => :git, :tag => "1.3.2", :revision => "bc1549ef1d8272f1cc2541c7f7f96180e8137204"
-  head "https://github.com/happn-tech/LocMapper.git", :using => :git, :branch => "develop"
+  url "https://github.com/happn-tech/LocMapper.git", using: :git, tag: "1.3.2", revision: "bc1549ef1d8272f1cc2541c7f7f96180e8137204"
+  revision 1
+  head "https://github.com/happn-tech/LocMapper.git", using: :git, branch: "develop"
 
-  depends_on :xcode => ["12.2", :build]
+  depends_on xcode: ["12.2", :build]
 
   def install
     system "swift", "build", "--disable-sandbox", "--configuration", "release", "--disable-automatic-resolution"
 
-    bin.install "./.build/release/locmapper"
+    bins = ["./.build/release/locmapper"]
+    bins.each do |b|
+      # Generate and install bash completion.
+      output = Utils.safe_popen_read(b, "--generate-completion-script", "bash")
+      (bash_completion/File.basename(b)).write output
+      # Generate and install zsh completion.
+      output = Utils.safe_popen_read(b, "--generate-completion-script", "zsh")
+      (zsh_completion/("_" + File.basename(b))).write output
+      # Generate and install fish completion.
+      output = Utils.safe_popen_read(b, "--generate-completion-script", "fish")
+      (fish_completion/File.basename(b)).write output
+
+      # Install the binary after completion is generated.
+      bin.install b
+    end
   end
 
   test do
