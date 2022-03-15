eaafec5ea5425c0086dd7403c12c360fc34eeaec
Frizlab <fload@me.com>
2022-03-15 20:09:56 +0100

    Fix officectl_odproxy installation

--- Formula/officectl.rb
+++ Formula/officectl.rb
@@ -2,7 +2,7 @@ class Officectl < Formula
   desc "Manage multiple user directories (LDAP, Google Apps, etc.)"
   homepage "https://github.com/happn-app/officectl"
   url "https://github.com/happn-app/officectl.git", using: :git, tag: "0.7.9", revision: "3a0f8ddf18dda546b4c011b7849b4295acb57d59"
-  revision 1
+  revision 2
   head "https://github.com/happn-app/officectl.git", using: :git, branch: "develop"
 
   depends_on xcode: ["12.4", :build]
@@ -12,7 +12,10 @@ class Officectl < Formula
     # headers search path and link options to swift build.
     system "swift", "build", "--disable-sandbox", "--configuration", "release", "--disable-automatic-resolution"
 
-    bins = ["./.build/release/officectl", "./.build/release/officectl_odproxy"]
+    # officectl_odproxy does not support completion script generation (yet),
+    # so we do not add it in this array and install it directly.
+    bin.install "./.build/release/officectl_odproxy"
+    bins = ["./.build/release/officectl"]
     bins.each do |b|
       # Generate and install bash completion.
       output = Utils.safe_popen_read(b, "--generate-completion-script", "bash")
