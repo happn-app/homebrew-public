47611c45ea64d259c3401252c18693d907567f8f
Frizlab <fload@me.com>
2022-11-22 20:40:17 +0100

    Upgrade LocMapper App to 1.3.5

--- Casks/locmapper-app.rb
+++ Casks/locmapper-app.rb
@@ -1,6 +1,6 @@
 cask 'locmapper-app' do
-  version '1.3.4'
-  sha256 '0914d77066de88021a795c835b9e7c1509608b98cbbeb82b2b04d16982d80083'
+  version '1.3.5'
+  sha256 '4a4bbba6d95d8b7f562b0aca048cd551e6010ba6d1b23f64a61fa7d7defc306e'
 
   url "https://github.com/happn-app/LocMapper/releases/download/LocMapper%2Frelease%2F#{version}/LocMapperApp.zip"
   name 'LocMapper'
